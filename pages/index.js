326ffdafa5cf45dcd3eba58524135fefd64dea2d
Fernando9200 <fernando91.sosa@gmail.com>
2021-11-09 14:58:46 -0300

    Ultimas modificações

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,71 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import { Toolbar } from '../components/toolbar';
+import imageUrlBuilder from '@sanity/image-url';
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
 
-export default function Home() {
+export default function Home({ posts }) {
+  const router = useRouter();
+  const [mappedPosts, setMappedPosts] = useState([]);
+
+  useEffect(() => {
+    if (posts.length) {
+      const imgBuilder = imageUrlBuilder({
+        projectId: 'b547fsql',
+        dataset: 'production',
+    });
+    
+    setMappedPosts(
+      posts.map(p => {
+        return {
+          ...p,
+          mainImage: imgBuilder.image(p.mainImage).width(500).height(250),
+        }
+      })
+    );
+  } else {
+      setMappedPosts([]);
+    }
+  }, [posts]);
+
   return (
-    <div>Hello!</div>
+    <div>
+      <Toolbar />
+      <div className={styles.main}>
+        <h1>Welcome to my blog</h1>
+
+        <h3>Recent posts: </h3>
+
+        <div className={styles.feed}>
+          {mappedPosts.length ? mappedPosts.map((p, index) => (
+            <div onClick={() => router.push(`/post/${p.slug.current}`)} key={index} className={styles.post}>
+              <h3>{p.title}</h3>
+              <img className={styles.mainImage} src={p.mainImage}/>
+            </div>
+          )) : <>No Posts Yet</>}
+        </div>
+      </div>
+    </div>
   );
 }
+
+export const getServerSideProps = async pageContext => {
+  const query = encodeURIComponent('*[ _type == "post" ]');
+  const url = `https://b547fsql.api.sanity.io/v1/data/query/production?query=${query}`;
+  const result = await fetch(url).then(res => res.json());
+
+  if (!result.result || !result.result.length) {
+    return {
+      props: {
+        posts: [],
+      }
+    }
+  } else {
+    return {
+      props: {
+        posts: result.result,
+      }
+    }
+  }
+};
