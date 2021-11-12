5cf028d364441bcdf1cb5210bafc2652ccf3661f
Fernando9200 <fernando91.sosa@gmail.com>
2021-11-12 14:45:43 -0300

    Projeto finalizado

--- components/toolbar.js
+++ components/toolbar.js
@@ -11,9 +11,9 @@ export const Toolbar = () => {
             <div onClick={() => router.push('/academic')}>Formação</div>
             <div onClick={() => router.push('/projects')}>Projetos</div>
             <div onClick={() => router.push('/contact')}>Contato</div>
-            <div class={styles.dropdown}>
-                <button class={styles.dropbtn}>Redes Sociais</button>
-                <div class={styles.dropdowncontent}>
+            <div className={styles.dropdown}>
+                <button className={styles.dropbtn}>Redes Sociais</button>
+                <div className={styles.dropdowncontent}>
                     <a onClick={() => window.location.href = 'https://www.workana.com/freelancer/0e4ed7aa5719ac3e8e4835b4dd4a9c92'}>Workana</a>
                     <a onClick={() => window.location.href = 'https://www.linkedin.com/in/fernando-carretto/'}>LinkedIn</a>
                     <a onClick={() => window.location.href = 'https://github.com/Fernando9200/novo-site'}>GitHub</a>
